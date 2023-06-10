0d782e6a33e441fecf33997a8a48f8be3aaf4635
Ilmari Vikstrom <ilmari.vikstrom@tietoevry.com>
2023-06-11 01:40:02 +0300

    Some menu work.

---
+++ src/menubase.py
@@ -0,0 +1,205 @@
+from typing import List, Tuple
+
+import pygame as pg
+
+from src.color_constants import GRAY5, WHITE, TRAIN_GREEN, TRAIN_YELLOW, TRAIN_RED, WHITESMOKE, GRAY50
+from src.config import Config
+from src.font import Font
+from src.utils import setup_logging
+
+logger = setup_logging(log_level=Config.log_level)
+
+
+class IndicatorStyle:
+    def __init__(self, fg_active_color: Tuple[int, int, int]=GRAY5, bg_active_color: Tuple[int, int, int]=TRAIN_YELLOW, fg_deactive_color: Tuple[int, int, int]=GRAY50, bg_deactive_color: Tuple[int, int, int]=GRAY5):
+        self.fg_active_color = fg_active_color
+        self.bg_active_color = bg_active_color
+        self.fg_deactive_color = fg_deactive_color
+        self.bg_deactive_color = bg_deactive_color
+
+
+    def as_dict(self):
+        return self.__dict__
+
+
+class YellowStyle(IndicatorStyle):
+    def __init__(self):
+        super().__init__(bg_active_color=TRAIN_YELLOW)
+
+
+class RedStyle(IndicatorStyle):
+    def __init__(self):
+        super().__init__(bg_active_color=TRAIN_RED)
+
+
+class GreenStyle(IndicatorStyle):
+    def __init__(self):
+        super().__init__(bg_active_color=TRAIN_GREEN)
+
+
+class WhiteStyle(IndicatorStyle):
+    def __init__(self):
+        super().__init__(bg_active_color=WHITESMOKE)
+
+
+class IndicatorItem:
+    def __init__(self, font: pg.Font, text: str, padding_spaces: int, style: IndicatorStyle, dest: Tuple[int, int]) -> None:
+        self.font: pg.Font = font
+        self.text: str = text
+        self.padding_spaces: int = padding_spaces
+        self.style = style
+        self.dest: Tuple[int, int] = dest
+
+        self.activated: bool = False
+        self.render()
+
+
+    def render(self):
+        if self.activated:
+            self.renderable = self.font.render(f"{self.text:^{len(self.text) + 2 * self.padding_spaces}}", True, self.style.fg_active_color, self.style.bg_active_color)
+        else:
+            self.renderable = self.font.render(f"{self.text:^{len(self.text) + 2 * self.padding_spaces}}", True, self.style.fg_deactive_color, self.style.bg_deactive_color)
+
+
+    def _activate(self):
+        self.activated = True
+        self.render()
+
+
+    def _deactivate(self):
+        self.activated = False
+        self.render()
+
+
+
+class Tooltip:
+    def __init__(self, text: str, dest: Tuple[int, int]) -> None:
+        self.surface: pg.Surface = Font.normal.render(text, True, WHITE)
+        self.dest: Tuple[int, int] = dest
+
+
+
+class BaseMenu:
+    def __init__(self, tooltip: Tooltip, indicator_items: List[IndicatorItem]) -> None:
+        self.tooltip: Tooltip = tooltip
+        self.indicator_items: List[IndicatorItem] = indicator_items
+
+
+    def get_activated_items(self) -> List[int]:
+        activated_items: List[int] = []
+        for i, indicator_item in enumerate(self.indicator_items):
+            if indicator_item.activated:
+                activated_items.append(i)
+        return activated_items
+
+
+    def set_text(self, text: str, item_index: int) -> None:
+        if item_index > len(self.indicator_items) - 1:
+            raise ValueError(f"Tried to set text for indicator item in index {item_index} when the maximum index is {len(self.indicator_items) - 1}")
+        self.indicator_items[item_index].text = text
+        self.indicator_items[item_index].render()
+
+
+    def deactivate_all(self) -> None:
+        for indicator_item in self.indicator_items:
+            indicator_item._deactivate()
+
+
+    def activate_item(self, item_to_activate: int) -> None:
+        if item_to_activate > len(self.indicator_items) - 1 or item_to_activate < 0:
+            logger.warning(f"Trying to activate menu item {item_to_activate} when there are only {len(self.indicator_items)} items available.")
+            return
+        for i, indicator_item in enumerate(self.indicator_items):
+            if i == item_to_activate:
+                indicator_item._activate()
+            else:
+                indicator_item._deactivate()
+
+
+    def activate_next_item(self) -> None:
+        activated_item = None
+        for i, indicator_item in enumerate(self.indicator_items):
+            if indicator_item.activated:
+                activated_item = i
+                break
+        final_item = len(self.indicator_items) - 1
+        if activated_item is None or activated_item == final_item:
+            next_item = final_item
+        else:
+            next_item = activated_item + 1
+        self.activate_item(next_item)
+
+
+    def activate_previous_item(self) -> None:
+        activated_item = None
+        for i, indicator_item in enumerate(self.indicator_items):
+            if indicator_item.activated:
+                activated_item = i
+                break
+        start_item = 0
+        if activated_item is None or activated_item == start_item:
+            next_item = start_item
+        else:
+            next_item = activated_item - 1
+        self.activate_item(next_item)
+
+
+    def draw(self, screen_surface: pg.Surface) -> None:
+        screen_surface.blit(self.tooltip.surface, self.tooltip.dest)
+        for indicator_item in self.indicator_items:
+            screen_surface.blit(indicator_item.renderable, indicator_item.dest)
+
+
+
+class VerticalMenu(BaseMenu):
+    def __init__(self, topleft: Tuple[int, int], title: str, menu_items: List[IndicatorItem]) -> None:
+        self.topleft: Tuple[int, int] = topleft
+        self.title: str = title
+        self.menu_items = menu_items
+        self.row_height: int = 20
+
+        self.tooltip = Tooltip(title, (self.topleft[0], self.topleft[1]))
+
+        self.indicator_items: List[IndicatorItem] = []
+
+        for i, menu_item in enumerate(menu_items):
+            self.indicator_items.append(
+                IndicatorItem(
+                    font=Font.normal,
+                    text=menu_item.text,
+                    padding_spaces=3,
+                    style=menu_item.style,
+                    dest=(self.topleft[0], self.topleft[1] + (i+1) * self.row_height)
+                )
+            )
+
+        super().__init__(self.tooltip, self.indicator_items)
+
+
+
+
+class HorizontalMenu(BaseMenu):
+    def __init__(self, topleft: Tuple[int, int], title: str, menu_items: List[IndicatorItem]) -> None:
+        self.topleft: Tuple[int, int] = topleft
+        self.title: str = title
+        self.menu_items = menu_items
+        self.row_height: int = 20
+        self.row_width: int = 128
+
+        self.tooltip = Tooltip(title, (self.topleft[0], self.topleft[1]))
+
+        self.indicator_items: List[IndicatorItem] = []
+
+        for i, menu_item in enumerate(menu_items):
+            self.indicator_items.append(
+                IndicatorItem(
+                    font=Font.normal,
+                    text=menu_item.text,
+                    padding_spaces=3,
+                    style=menu_item.style,
+                    dest=(self.topleft[0] + i * self.row_width, self.topleft[1] + 1 * self.row_height)
+                )
+            )
+
+        super().__init__(self.tooltip, self.indicator_items)
+
--- src/menus.py
+++ src/menus.py
@@ -1,196 +1,64 @@
 from typing import List, Tuple
 
-import pygame as pg
-
-from src.color_constants import GRAY5, WHITE, TRAIN_GREEN, TRAIN_YELLOW, TRAIN_RED, WHITESMOKE
 from src.config import Config
 from src.font import Font
-from src.indicator_item import IndicatorItem
+from src.menubase import IndicatorItem, GreenStyle, RedStyle, WhiteStyle, YellowStyle, HorizontalMenu, VerticalMenu
 from src.utils import setup_logging
 
 logger = setup_logging(log_level=Config.log_level)
 
-class Tooltip:
-    def __init__(self, text: str, dest: Tuple[int, int]) -> None:
-        self.surface: pg.Surface = Font.small.render(text, True, WHITE)
-        self.dest: Tuple[int, int] = dest
 
-
-
-class BaseMenu:
-    def __init__(self, tooltip: Tooltip, indicator_items: List[IndicatorItem]) -> None:
-        self.tooltip: Tooltip = tooltip
-        self.indicator_items: List[IndicatorItem] = indicator_items
-
-    def get_activated_items(self) -> List[int]:
-        activated_items: List[int] = []
-        for i, indicator_item in enumerate(self.indicator_items):
-            if indicator_item.activated:
-                activated_items.append(i)
-        return activated_items
-
-
-    def deactivate_all(self) -> None:
-        for indicator_item in self.indicator_items:
-            indicator_item._deactivate()
-
-
-    def activate_item(self, item_to_activate: int) -> None:
-        if item_to_activate > len(self.indicator_items) - 1 or item_to_activate < 0:
-            logger.warning(f"Trying to activate menu item {item_to_activate} when there are only {len(self.indicator_items)} items available.")
-            return
-        for i, indicator_item in enumerate(self.indicator_items):
-            if i == item_to_activate:
-                indicator_item._activate()
-            else:
-                indicator_item._deactivate()
-
-
-    def activate_next_item(self) -> None:
-        activated_item = None
-        for i, indicator_item in enumerate(self.indicator_items):
-            if indicator_item.activated:
-                activated_item = i
-                break
-        final_item = len(self.indicator_items) - 1
-        if activated_item is None or activated_item == final_item:
-            next_item = final_item
-        else:
-            next_item = activated_item + 1
-        self.activate_item(next_item)
-
-
-    def activate_previous_item(self) -> None:
-        activated_item = None
-        for i, indicator_item in enumerate(self.indicator_items):
-            if indicator_item.activated:
-                activated_item = i
-                break
-        start_item = 0
-        if activated_item is None or activated_item == start_item:
-            next_item = start_item
-        else:
-            next_item = activated_item - 1
-        self.activate_item(next_item)
-
-
-    def draw(self, screen_surface: pg.Surface) -> None:
-        screen_surface.blit(self.tooltip.surface, self.tooltip.dest)
-        for indicator_item in self.indicator_items:
-            screen_surface.blit(indicator_item.rendered, indicator_item.dest)
-
-
-
-class BuildPurgeMenu(BaseMenu):
+class BuildPurgeMenu(VerticalMenu):
     def __init__(self, topleft: Tuple[int, int]) -> None:
-        self.topleft: Tuple[int, int] = topleft
-        self.row_height = 20
-
-        self.tooltip = Tooltip("L-SHIFT", (self.topleft[0] + 32, self.topleft[1]))
-
-        self.first = IndicatorItem(
-            font=Font.normal,
-            text="BUILD",
-            padding_spaces=2,
-            activated_text_color=GRAY5,
-            activated_bg_color=TRAIN_YELLOW,
-            dest=(self.topleft[0], self.topleft[1] + 1 * self.row_height)
-        )
-        self.second = IndicatorItem(
-            font=Font.normal,
-            text="PURGE",
-            padding_spaces=2,
-            activated_text_color=GRAY5,
-            activated_bg_color=TRAIN_RED,
-            dest=(self.topleft[0], self.topleft[1] + 2 * self.row_height)
-        )
-        super().__init__(self.tooltip, [self.first, self.second])
+        title = "L-SHIFT"
+        menu_items: List[IndicatorItem] = [
+            IndicatorItem(text="BUILD", padding_spaces=2, style=YellowStyle(), dest=topleft, font=Font.small),
+            IndicatorItem(text="PURGE", padding_spaces=2, style=RedStyle(), dest=topleft, font=Font.small)
+        ]
+        super().__init__(topleft, title, menu_items)
 
 
 
-class EditTestMenu(BaseMenu):
+class EditTestMenu(HorizontalMenu):
     def __init__(self, topleft: Tuple[int, int]) -> None:
-        self.topleft: Tuple[int, int] = topleft
-        self.row_height = 20
-
-        self.tooltip = Tooltip("SPACEBAR", (self.topleft[0] + 96, self.topleft[1]))
-
-        self.first = IndicatorItem(
-            font=Font.normal,
-            text="EDIT",
-            padding_spaces=3,
-            activated_text_color=GRAY5,
-            activated_bg_color=WHITESMOKE,
-            dest=(self.topleft[0], self.topleft[1] + 1 * self.row_height)
-        )
-        self.second = IndicatorItem(
-            font=Font.normal,
-            text="TEST",
-            padding_spaces=3,
-            activated_text_color=GRAY5,
-            activated_bg_color=WHITESMOKE,
-            dest=(self.topleft[0] + 128, self.topleft[1] + 1 * self.row_height)
-        )
-        super().__init__(self.tooltip, [self.first, self.second])
+        title = "SPACEBAR"
+        menu_items: List[IndicatorItem] = [
+            IndicatorItem(text="EDIT", padding_spaces=3, style=WhiteStyle(), dest=topleft, font=Font.small),
+            IndicatorItem(text="TEST", padding_spaces=3, style=WhiteStyle(), dest=topleft, font=Font.small)
+        ]
+        super().__init__(topleft, title, menu_items)
 
 
 
-
-class RunningCrashedCompleteMenu(BaseMenu):
+class RunningCrashedCompleteMenu(VerticalMenu):
     def __init__(self, topleft: Tuple[int, int]) -> None:
-        self.topleft: Tuple[int, int] = topleft
-        self.row_height = 20
-
-        self.tooltip = Tooltip("LEVEL STATUS", (self.topleft[0] + 36, self.topleft[1]))
-
-        self.first = IndicatorItem(
-            font=Font.normal,
-            text="RUNNING",
-            padding_spaces=3,
-            activated_text_color=GRAY5,
-            activated_bg_color=TRAIN_YELLOW,
-            dest=(self.topleft[0], self.topleft[1] + 1 * self.row_height)
-        )
-        self.second = IndicatorItem(
-            font=Font.normal,
-            text="CRASHED",
-            padding_spaces=3,
-            activated_text_color=GRAY5,
-            activated_bg_color=TRAIN_RED,
-            dest=(self.topleft[0], self.topleft[1] + 2 * self.row_height)
-        )
-        self.third = IndicatorItem(
-            font=Font.normal,
-            text="COMPLETED",
-            padding_spaces=2,
-            activated_text_color=GRAY5,
-            activated_bg_color=TRAIN_GREEN,
-            dest=(self.topleft[0], self.topleft[1] + 3 * self.row_height)
-        )
-        super().__init__(self.tooltip, [self.first, self.second, self.third])
+        title = "LEVEL STATUS"
+        menu_items: List[IndicatorItem] = [
+            IndicatorItem(text="RUNNING", padding_spaces=3, style=YellowStyle(), dest=topleft, font=Font.small),
+            IndicatorItem(text="CRASHED", padding_spaces=3, style=RedStyle(), dest=topleft, font=Font.small),
+            IndicatorItem(text="COMPLETED", padding_spaces=3, style=GreenStyle(), dest=topleft, font=Font.small),
+        ]
+        super().__init__(topleft, title, menu_items)
 
 
 
-class LevelMenu(BaseMenu):
+class LevelMenu(VerticalMenu):
     def __init__(self, topleft: Tuple[int, int], num_levels: int) -> None:
-        self.topleft: Tuple[int, int] = topleft
-        self.num_levels = num_levels
-        self.row_height = 20
+        title = "LEVEL SELECT"
+        menu_items: List[IndicatorItem] = [
+            IndicatorItem(
+                text=f"Level {i}",
+                style=YellowStyle(),
+                padding_spaces=2,
+                dest=topleft,
+                font=Font.small
+            ) for i in range(num_levels)
+        ]
+        super().__init__(topleft, title, menu_items)
 
-        self.tooltip = Tooltip("LEVEL SELECT", (self.topleft[0] + 36, self.topleft[1]))
 
-        self.levels: List[IndicatorItem] = []
 
-        for i in range(num_levels):
-            self.levels.append(
-                IndicatorItem(
-                    font=Font.normal,
-                    text=f"LEVEL {i}",
-                    padding_spaces=3,
-                    activated_text_color=GRAY5,
-                    activated_bg_color=TRAIN_YELLOW,
-                    dest=(self.topleft[0], self.topleft[1] + (i+1) * self.row_height)
-                )
-            )
-
-        super().__init__(self.tooltip, self.levels)
+class InfoMenu(VerticalMenu):
+    def __init__(self ,topleft: Tuple[int, int], tooltip_text: str, value: str) -> None:
+        indicator_item = IndicatorItem(Font.small, value, 3, YellowStyle(), topleft)
+        super().__init__(topleft, tooltip_text, [indicator_item])
